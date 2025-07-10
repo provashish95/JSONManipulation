b2742570d63da3e96621ca6632d86cac4a015fcd
provashish95 <roytonmoy95@gmail.com>
2025-07-10 14:46:05 +0600

    JSONArray

---
+++ src/main/java/JSONManipulation/JSONArrayList.java
@@ -0,0 +1,33 @@
+package JSONManipulation;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class JSONArrayList {
+    public static void main(String[] args) throws IOException, ParseException {
+        JSONParser jsonParser = new JSONParser();
+        JSONArray jsonArray = (JSONArray) jsonParser.parse(new FileReader("./src/main/resources/emp.json"));
+
+        JSONObject empObj = new JSONObject();
+        empObj.put("name", "Roy");
+        empObj.put("address", "Dhaka");
+        empObj.put("passion", "SEO");
+
+        //System.out.println(empObj);
+        jsonArray.add(empObj);
+
+        System.out.println(jsonArray);
+
+        FileWriter fw = new FileWriter("./src/main/resources/emp.json");
+        fw.write(jsonArray.toJSONString());
+        fw.flush();
+        fw.close();
+
+    }
+}
