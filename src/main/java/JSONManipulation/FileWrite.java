78026cc7c1169caf0070536c52b0c25af26eee7c
provashish95 <roytonmoy95@gmail.com>
2025-07-10 12:05:33 +0600

    File read & File write function implement

--- src/main/java/JSONManipulation/FileWrite.java
+++ src/main/java/JSONManipulation/FileWrite.java
@@ -1,14 +1,30 @@
 package JSONManipulation;
 
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 
 public class FileWrite {
     public static void main(String[] args) throws IOException {
 
-        FileWriter fw = new FileWriter("./src/main/resources/test.txt");
-        fw.write("Hello java and sqa");
+        writeFile("./src/main/resources/test.txt", true);
+        readFile("./src/main/resources/test.txt");
+
+    }
+
+    public static void writeFile(String filename, boolean append) throws IOException {
+
+        FileWriter fw = new FileWriter(filename, append);
+
+        fw.write("\n");
+        fw.write("Hello java");
         fw.close();
+    }
+
+    public static void readFile(String fileUrl) throws IOException {
+        FileReader fr = new FileReader(fileUrl);
+        int character = fr.read();
+        System.out.println(character);
     }
 
 }
