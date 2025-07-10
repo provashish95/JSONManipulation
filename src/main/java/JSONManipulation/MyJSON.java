3712e8de2747241e828d542164e96fd6dbb4ad6e
provashish95 <roytonmoy95@gmail.com>
2025-07-10 12:42:18 +0600

    object write

--- src/main/java/JSONManipulation/MyJSON.java
+++ src/main/java/JSONManipulation/MyJSON.java
@@ -1,5 +1,34 @@
 package JSONManipulation;
 
-public class MyJSON {
+import org.json.simple.JSONObject;
 
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class MyJSON {
+    public static void main(String[] args) throws IOException {
+        JSONObject empObj = new JSONObject();
+
+        empObj.put("name", "provashish");
+        empObj.put("designation", "SQA");
+        empObj.put("address", "dhaka");
+
+
+        JSONObject hobbyObj = new JSONObject();
+
+        hobbyObj.put("language-1", "C programming");
+        hobbyObj.put("language-2", "JavaScript");
+        hobbyObj.put("language-3", "Java");
+
+        empObj.put("hobby", hobbyObj);
+
+        //System.out.println(empObj);
+
+        FileWriter fw = new FileWriter("./src/main/resources/employee.json");
+
+        fw.write(empObj.toJSONString());
+        fw.flush();
+        fw.close();
+
+    }
 }
