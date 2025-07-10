2aad50cee18a07c26c34cb0cab1684aba8749ad6
provashish95 <roytonmoy95@gmail.com>
2025-07-10 12:12:19 +0600

    update class name

--- src/main/java/JSONManipulation/FileReadWrite.java
+++ src/main/java/JSONManipulation/FileReadWrite.java
@@ -4,7 +4,7 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 
-public class FileWrite {
+public class FileReadWrite {
     public static void main(String[] args) throws IOException {
 
         //writeFile("./src/main/resources/test.txt", true);
